2cce8522bed3b6bb107d8014fdf0010aeaf45c89
adnaneIIIII <adnane.elotmani@usmba.ac.ma>
2025-07-19 18:11:13 +0100

    Initial checkout mnts done ICON contact suppor thank you slash

--- components/_home/header.tsx
+++ components/_home/header.tsx
@@ -25,7 +25,7 @@ export default function Header() {
     >
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center py-4">
-          <Link href={"/home"}>
+          <Link href={"/"}>
             <Image
               src={"/Mntdigitals-b.png"}
               width={120}
